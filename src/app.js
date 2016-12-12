dbf86f0e1132a7c9225e1cdaa5862ed8a1a3b61a
Jesus J.M <jesusgraficap@gmail.com>
2016-12-12 19:48:02 +0100

    fitst actions and provider

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Ecommerce from './components/ecommerce';
+import route from './modules/route';
+
+console.log(route(undefined, { type: 'XXX' }));
+console.log(route('catalog', { type: 'SET_ROUTE', route: 'cart' }));
 
 ReactDom.render(<Ecommerce />, document.getElementById('app'));
---
+++ src/modules/route/actions.js
@@ -0,0 +1,21 @@
+export const SET_ROUTE = 'SET_ROUTE';
+
+function setRoute(newRoute) {
+    return {
+        type: SET_ROUTE,
+        route: newRoute
+    }
+}
+
+export function goToCatalog() {
+    return setRoute('catalog');
+}
+export function goToCart() {
+    return setRoute('cart');
+}
+export function goToCheckout() {
+    return setRoute('checkout');
+}
+export function goToThankYou() {
+    return setRoute('thank-you');
+}
